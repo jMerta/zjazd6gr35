9f42a508e6116f51f13006270e0a95e556a4eb91
jmerta <mer.kuba@gmail.com>
2023-12-10 15:33:29 +0100

    ComponentB changes

--- src/main/java/com/example/zjazd6gr35/ComponentB.java
+++ src/main/java/com/example/zjazd6gr35/ComponentB.java
@@ -1,11 +1,21 @@
 package com.example.zjazd6gr35;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Component(value = "test")
 public class ComponentB {
 
+    private final ComponentA componentA;
+
+    public ComponentB(ComponentA componentA) {
+        this.componentA = componentA;
+    }
+
     public void printName(){
         System.out.println("Nazwa komponentu: " + this.getClass().getSimpleName());
+        System.out.println("Nazwa zależności: " + componentA.getClass().getSimpleName());
+        componentA.printName();
     }
 }
